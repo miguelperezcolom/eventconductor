bab650087ec72fc401b08b6880ebe1502ec85b36
Miguel <miguelperezcolom@gmail.com>
2026-03-20 07:45:57 +0100

    Add demo.

---
+++ demo/shell/src/test/java/io/mateu/workflow/shell/ShellApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mateu.workflow.shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demo/users-service/src/main/java/io/mateu/workflow/usersservice/UsersServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mateu.workflow.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+}
