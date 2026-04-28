b2becf711561bc4a457b712d53e0cb9a5c526553
Miguel <miguelperezcolom@gmail.com>
2026-04-28 11:59:24 +0200

    wip.

--- modules/workflow-engine/src/main/java/io/mateu/workflow/domain/aggregates/Step.java
+++ modules/workflow-engine/src/main/java/io/mateu/workflow/domain/aggregates/Step.java
@@ -2,6 +2,8 @@ package io.mateu.workflow.domain.aggregates;
 
 import io.mateu.uidl.annotations.*;
 import io.mateu.uidl.interfaces.Identifiable;
+import io.mateu.workflow.infra.in.ui.suppliers.WorkflowDefinitionIdLabelSupplier;
+import io.mateu.workflow.infra.in.ui.suppliers.WorkflowDefinitionIdOptionsSupplier;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.With;
@@ -18,6 +20,7 @@ public record Step(
         StepType type,
         @NotEmpty
         String name,
+        @HiddenInList
         String description,
         @Section(value = "Precondition", style = "width: 25%;")
         @HiddenInList
@@ -25,16 +28,22 @@ public record Step(
         @Section(value = "Execution", style = "width: 25%;")
         boolean parallel,
         @HiddenInList
+        @Hidden("state['steps-type'] != 'ACTION'")
         String topic,
         @HiddenInList
+        @Hidden("state['steps-type'] != 'USER_TASK'")
         String formId,
-        @Section(value = "Reliability", style = "width: 25%;")
         @HiddenInList
-        boolean rollbackable,
+        @Hidden("state['steps-type'] != 'PROCESS'")
+        @Lookup(search = WorkflowDefinitionIdOptionsSupplier.class, label = WorkflowDefinitionIdLabelSupplier.class)
+        String childWorkflowDefinitionId,
+        @Section(value = "Reliability", style = "width: 25%;")
         @HiddenInList
         long timeout,
         @HiddenInList
         int retries,
+        @HiddenInList
+        boolean rollbackable,
         @HiddenInList
         @Hidden("!state['steps-rollbackable']")
         String compensationStepId
