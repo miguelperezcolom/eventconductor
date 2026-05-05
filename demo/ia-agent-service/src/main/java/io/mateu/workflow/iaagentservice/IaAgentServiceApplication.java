533bb2602ccb019609c1dc9f023798e812fbdd30
Miguel <miguelperezcolom@gmail.com>
2026-05-05 17:10:30 +0200

    Add agent project.

---
+++ demo/ia-agent-service/src/main/java/io/mateu/workflow/iaagentservice/IaAgentServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mateu.workflow.iaagentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaAgentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaAgentServiceApplication.class, args);
+    }
+
+}
